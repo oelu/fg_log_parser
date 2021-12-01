7fd373e60c4b2103723a30bf15975ddcf854f83f
Jan Kraus <jan.kraus@shi-g.com>
2021-12-01 16:17:54 +0100

    Updated Script to Python3

--- fg_log_parser.py
+++ fg_log_parser.py
@@ -184,7 +184,7 @@ def get_communication_matrix(logfile,
             """
 
             # check if necessary fields are in first line
-            if linecount is 1 and not noipcheck:
+            if linecount == 1 and not noipcheck:
                 # print error message if srcip or dstip are missing
                 if not check_log_format(line, srcipfield, dstipfield):
                     log.error("srcipfield or dstipfield not in line: %s ", linecount)
@@ -263,13 +263,13 @@ def print_communication_matrix(matrix, indent=0):
                     count
                         1
     """
-    for key, value in matrix.iteritems():
+    for key, value in matrix.items():
         # values are printed with 4 whitespace indent
-        print '    ' * indent + str(key)
+        print('    ' * indent + str(key))
         if isinstance(value, dict):
             print_communication_matrix(value, indent+1)
         else:
-            print '    ' * (indent+1) + str(value)
+            print('    ' * (indent+1) + str(value))
     return None
 
 def print_communication_matrix_as_csv(matrix, countbytes=False, showaction=False):
@@ -290,7 +290,7 @@ def print_communication_matrix_as_csv(matrix, countbytes=False, showaction=False
 
     """
     # Header
-    print "srcip;dstip;dport;proto;count;action;sentbytes;rcvdbytes"
+    print("srcip;dstip;dport;proto;count;action;sentbytes;rcvdbytes")
     for srcip in matrix.keys():
         for dstip in matrix.get(srcip):
             for dport in matrix[srcip][dstip].keys():
@@ -303,9 +303,9 @@ def print_communication_matrix_as_csv(matrix, countbytes=False, showaction=False
                     if countbytes:
                             rcvdbytes = matrix[srcip][dstip][dport][proto].get("rcvdbytes")
                             sentbytes = matrix[srcip][dstip][dport][proto].get("sentbytes")
-                            print "%s;%s;%s;%s;%s;%s;%s;%s" % (srcip, dstip, dport, proto, count, action, sentbytes, rcvdbytes)
+                            print("%s;%s;%s;%s;%s;%s;%s;%s" % (srcip, dstip, dport, proto, count, action, sentbytes, rcvdbytes))
                     else:
-                        print "%s;%s;%s;%s;%s;%s" % (srcip, dstip, dport, proto, count, action)
+                        print("%s;%s;%s;%s;%s;%s" % (srcip, dstip, dport, proto, count, action))
 
 def main():
     """
@@ -345,7 +345,7 @@ def main():
 
     # check if logfile argument is present
     if logfile is None:
-        print __doc__
+        print(__doc__)
         sys.exit(1)
 
     # parse log
