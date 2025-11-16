3480fa3a6514f23cd7830f488b9f5d8bf2514ec6
Oelu <oelu@local>
2025-11-16 20:56:02 +0100

    updated to python3

--- fg_log_parser.py
+++ fg_log_parser.py
@@ -265,7 +265,7 @@ def print_communication_matrix(matrix, indent=0):
                     count
                         1
     """
-    for key, value in matrix.iteritems():
+    for key, value in matrix.items():
         # values are printed with 4 whitespace indent
         print('    ' * indent + str(key))
         if isinstance(value, dict):
